e99cea47bd0aabef83305c389b82a410561cf3fe
Akshay Rathi <akshayrathi96@gmail.com>
2020-06-03 20:00:13 +0530

    Add files via upload

--- Server/server.js
+++ Server/server.js
@@ -10,23 +10,31 @@ const app = express()
 // our server instance
 const server = http.createServer(app)
 
+// socketIo instance
 const io = socketIO(server)
 
+// connecting website to socketIo
 io.on('connection', socket => {
+
+  // notifying teacher when a student joins the server
   socket.broadcast.emit("Userconnected", "userConnected")
-  console.log('connected')
-   socket.on('ChildToServer', (Data) => {
+
+  // receiving data from children
+  socket.on('ChildToServer', (Data) => {
+
+    // formula to calculate WPM(words per minute)
     const currtime = (new Date() / 1000).toFixed(0)
     const WPM = (((Data.Characters.length) * 60) / (5 * (currtime - (Data.Wpm)))).toFixed(0)
     Data.Wpm = WPM
+
+    // sending calculated WPM to teacher
     socket.broadcast.emit("ServerToTeacher", Data)
   })
-
   socket.on('disconnect', () => {
-    console.log("Disconnected")
-    socket.broadcast.emit("DisconnectedFromServer","Userdisconnected")
+    
+    // notifying teacher when a student leaves the server
+    socket.broadcast.emit("DisconnectedFromServer", "Userdisconnected")
   })
 })
-
 
 server.listen(port, () => console.log(`Listening on port ${port}`))
